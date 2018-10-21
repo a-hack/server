435a7ea4a10150808c65a57e334dc7dfd208f14e
Jake <jakeincanada@icloud.com>
2018-10-21 14:33:36 +0000

    Fix request name

--- api/cors.js
+++ api/cors.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 module.exports = function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    resp.setHeader("Access-Control-Allow-Headers", req.getHeader("Access-Control-Request-Headers"));
+    res.setHeader("Access-Control-Allow-Headers", req.getHeader("Access-Control-Request-Headers"));
     next();
 };
