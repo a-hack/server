1af22c9790d10b95f46f089cacb151ccc30fe468
Jake <jakeincanada@icloud.com>
2018-10-21 18:59:30 +0000

    License under GPL

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+/**
+ * A customizable data visualization web application.
+ * Copyright (C) 2018  Jacob MacDonald, Jacob Martin, Patrick Gingras,
+ * Michael Dysart, Aweys Ahmed, Hassan Salami, Aritz Joseph Beobide-Cardinal
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
